777a3d25bebd5cd5d83b8419c41db152203bcbff
Mohammad Wasi <150406862+MohammadWasi12345@users.noreply.github.com>
2023-12-03 22:30:49 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var countryName = "pakistan";
+var age = 18;
+if (countryName === "pakistan") {
+    if (age < 18) {
+        console.log("here is your ticket");
+    }
+    else {
+        console.log("age restriction");
+    }
+}
+else {
+    console.log("invalid country");
+}
---
+++ main.ts
@@ -0,0 +1,14 @@
+let countryName = "pakistan";
+let age = 18;
+if (countryName==="pakistan"){
+    if(age >=18){
+        console.log("here is your ticket");
+    }
+    else{
+    console.log("age restriction");
+        
+    }
+}
+else{
+    console.log("invalid country");
+}
